10f9a6811a368e9030a36bf9cf4e364cbd97cd0a
Saul Lopez <slopez15@asu.edu>
2018-10-16 19:16:31 -0700

    noted in template promise on how to run/use it.

--- PromisesAsync/templatePromise.js
+++ PromisesAsync/templatePromise.js
@@ -2,13 +2,17 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 
-var ROOT_DIR = "html/";
+var ROOT_DIR = "/html/";
 
 function readFileAndReplace(output, file, replaceStr) {
     return new Promise(function(resolve, reject) {
-	fs.readFile(ROOT_DIR + file, function (err, data) {
+	//console.log(__dirname);
+	//console.log(ROOT_DIR);
+	//console.log(file);
+	//console.log(__dirname + ROOT_DIR + file);
+	fs.readFile(__dirname + ROOT_DIR + file, function (err, data) {
 	    if (err) {
-		reject({ status : 400, theOutput : "<HTML><HEAD><Title>Error!</Title></HEAD><Body>Unable to process the request</Body</HTML>" });
+		reject({ status : 400, theOutput : "<HTML><HEAD><Title>Error!</Title></HEAD><Body>Unable to process the request" + err +"</Body</HTML>" });
 	    } else if (replaceStr != null) {
 		resolve({ status : 200, theOutput : output.theOutput.replace(replaceStr, data.toString()) });
 	    } else {
@@ -38,8 +42,20 @@ http.createServer(function (req, res) {
 	res.writeHead(200, { 'Content-type': 'text/html' });
 	res.end(result.theOutput);
     }, function(error) {
-	console.log("Some error in the promise!");
+	console.log("Some error in the promise!:"); //+ JSON.stringify(error));
 	res.writeHead(error.status, { 'Content-type': 'text/html' });
 	res.end(error.theOutput);
     });
 }).listen(8088);
+
+/*
+//testing:
+GET: localhost:8088\
+	err: __dirname\html\bannerundefined.html'
+How to run:
+	http://localhost:8088/?banner=1&left=1&footer=1&main=1
+	based on names of files.
+Doing?
+	takes template.html
+	then replace certain parts (words like MAIN) and replace it with an entire file's data like main1. essentially a component.
+*/
